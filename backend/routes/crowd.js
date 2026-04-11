e8f5f3499210f1b144c69ebef948034b649cf76c
SamyogGhimire <samyog.ghimire25@gmail.com>
2026-04-11 19:23:27 +0530

    add QR scanning endpoint

--- backend/routes/crowd.js
+++ backend/routes/crowd.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { checkIn, checkOut, getLiveCrowd, getAttendanceHistory, getHourlyStats } = require('../controllers/crowdController');
 
+const {
+  scanQR,             
+  checkIn,
+  checkOut,
+  getLiveCrowd,
+  getAttendanceHistory,
+  getHourlyStats,
+} = require('../controllers/crowdController');
+
+router.get('/live', getLiveCrowd);
 router.post('/checkin', checkIn);
 router.post('/checkout', checkOut);
-router.get('/live', getLiveCrowd);
 router.get('/history', getAttendanceHistory);
 router.get('/hourly', getHourlyStats);
+router.post('/scan', scanQR);                  
 
 module.exports = router;
